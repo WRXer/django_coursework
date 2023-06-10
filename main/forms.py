6551c12c15d7a1b29309d8a4cd99122dcd698f91
WRXer <king_311@mail.ru>
2023-06-10 12:57:30 +0400

    написал вьюшку для управления рассылками

---
+++ main/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Mailing
+
+class MailingForm(forms.ModelForm):
+    class Meta:
+        model = Mailing
+        fields = ['']
--- main/views.py
+++ main/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views import generic
+from .forms import MailingForm
+from main.models import Mailing
+
 
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
+
+
+class MailingCreateView(generic.CreateView):
+    model = Mailing
+    form_class = MailingForm
+    template_name = 'main/create_mailing.html'
+
+
+class MailingDetailView(generic.DetailView):
+    model = Mailing
+    template_name = 'main/mailing_detail.html'
+
+
+class MailingUpdateView(generic.UpdateView):
+    model = Mailing
+    form_class = MailingForm
+    template_name = 'main/update_mailing.html'
+
+    def form_valid(self, form):
+        self.object = form.save()
+        return redirect(self.object.get_absolute_url())
+
+
+class MailingDeleteView(generic.DeleteView):
+    model = Mailing  # Модель
+    success_url = reverse_lazy('main/mailing_list.html')  # Адрес для перенаправления после успешного удаления
